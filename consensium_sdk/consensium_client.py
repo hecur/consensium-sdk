b8c344c4412a670c5f63edfeae9c7ed197e0d5bc
kewkie <kewkie@gmail.com>
2024-11-26 15:38:37 +0000

    Also return status code.

--- consensium_sdk/consensium_client.py
+++ consensium_sdk/consensium_client.py
@@ -24,19 +24,24 @@ class OwnerClient:
         self._headers = {"Authorization": f"Bearer {self._owner_token}"}
 
     def create_project(self, project: dict):
-        return requests.post(f"{self._base_url}/projects", headers=self._headers, json=project).json()
+        response = requests.post(f"{self._base_url}/projects", headers=self._headers, json=project)
+        return response.json(), response.status_code
 
     def update_policy(self, project_id: str, policy_document: str):
-        return requests.put(f"{self._base_url}/projects/{project_id}/policy", headers=self._headers, json={"policy_document": policy_document}).json()
+        response = requests.put(f"{self._base_url}/projects/{project_id}/policy", headers=self._headers, json={"policy_document": policy_document})
+        return response.json(), response.status_code
     
     def add_predictor(self, project_id: str, predictor_ids: list[str]):
-        return requests.put(f"{self._base_url}/projects/{project_id}/predictors", headers=self._headers, json={"predictor_ids": predictor_ids}).json()
+        response = requests.put(f"{self._base_url}/projects/{project_id}/predictors", headers=self._headers, json={"predictor_ids": predictor_ids})
+        return response.json(), response.status_code
     
     def add_moderator(self, project_id: str, moderator_ids: list[str]):
-        return requests.put(f"{self._base_url}/projects/{project_id}/moderators", headers=self._headers, json={"moderator_ids": moderator_ids}).json()
+        response = requests.put(f"{self._base_url}/projects/{project_id}/moderators", headers=self._headers, json={"moderator_ids": moderator_ids})
+        return response.json(), response.status_code
 
     def put_instance(self, project_id: str, features: dict):
-        return requests.post(f"{self._base_url}/projects/{project_id}/instances", headers=self._headers, json={"features": features}).json()
+        response = requests.post(f"{self._base_url}/projects/{project_id}/instances", headers=self._headers, json={"features": features})
+        return response.json(), response.status_code
 
 
 class PredictorClient:
@@ -50,13 +55,17 @@ class PredictorClient:
         self._headers = {"Authorization": f"Bearer {self._predictor_token}"}
 
     def list_projects(self):
-        return requests.get(f"{self._base_url}/projects", headers=self._headers).json()
+        response = requests.get(f"{self._base_url}/projects", headers=self._headers)
+        return response.json(), response.status_code
 
     def get_project(self, project_id: str):
-        return requests.get(f"{self._base_url}/projects/{project_id}", headers=self._headers).json()
+        response = requests.get(f"{self._base_url}/projects/{project_id}", headers=self._headers)
+        return response.json(), response.status_code
 
     def get_instance_batch(self, project_id: str, last_instance_id: str, batch_size: int):
-        return requests.get(f"{self._base_url}/projects/{project_id}/instances?last_instance_id={last_instance_id}&batch_size={batch_size}", headers=self._headers).json()
+        response = requests.get(f"{self._base_url}/projects/{project_id}/instances?last_instance_id={last_instance_id}&batch_size={batch_size}", headers=self._headers)
+        return response.json(), response.status_code
 
     def put_prediction(self, project_id: str, instance_id: str, prediction_yes: float):
-        return requests.post(f"{self._base_url}/projects/{project_id}/instances/{instance_id}/predictions", headers=self._headers, json={"prediction_yes": prediction_yes})
+        response = requests.post(f"{self._base_url}/projects/{project_id}/instances/{instance_id}/predictions", headers=self._headers, json={"prediction_yes": prediction_yes})
+        return response.json(), response.status_code
