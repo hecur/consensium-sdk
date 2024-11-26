d4ac5da3b2c73dc9206b7d40b8c2da0e13041dee
kewkie <kewkie@gmail.com>
2024-11-26 11:31:28 +0000

    Add project owner calls.

--- consensium_client/consensium_client.py
+++ consensium_client/consensium_client.py
@@ -14,35 +14,45 @@ class ModeratorClient:
 
 
 class OwnerClient:
-    def __init__(self, base_url=base_url):
-        pass
+    _owner_token: str
+    _base_url: str
+    _headers: dict
 
-    def create_project(self, acceptance_policy):
-        pass
+    def __init__(self, owner_token, base_url=base_url):
+        self._owner_token = owner_token
+        self._base_url = base_url
+        self._headers = {"Authorization": f"Bearer {self._owner_token}"}
 
-    def update_project(self, project_id, acceptance_policy):
-        pass
+    def create_project(self, policy):
+        return requests.post(f"{self._base_url}/projects", headers=self._headers, json={"policy": policy}).json()
+
+    def update_policy(self, project_id, policy):
+        return requests.put(f"{self._base_url}/projects/{project_id}/policy", headers=self._headers, json={"policy": policy}).json()
+    
+    def 
 
     def put_instance(self, project_id, features):
-        pass
+        return requests.post(f"{self._base_url}/projects/{project_id}/instances", headers=self._headers, json={"features": features}).json()
 
 
 class PredictorClient:
     _predictor_token: str
     _base_url: str
-
+    _headers: dict
+    
     def __init__(self, predictor_token, base_url=base_url):
         self._predictor_token = predictor_token
         self._base_url = base_url
+        self._headers = {"Authorization": f"Bearer {self._predictor_token}"}
 
     def list_projects(self):
-        return requests.get(f"{self._base_url}/projects", headers={"Authorization": f"Bearer {self._predictor_token}"}).json()
+        return requests.get(f"{self._base_url}/projects", headers=self._headers).json()
 
     def get_project(self, project_id):
-        return requests.get(f"{self._base_url}/projects/{project_id}", headers={"Authorization": f"Bearer {self._predictor_token}"}).json()
+        return requests.get(f"{self._base_url}/projects/{project_id}", headers=self._headers).json()
 
     def get_instance_batch(self, project_id, last_instance_id, batch_size):
-        return requests.get(f"{self._base_url}/projects/{project_id}/instances?last_instance_id={last_instance_id}&batch_size={batch_size}", headers={"Authorization": f"Bearer {self._predictor_token}"}).json()
+        return requests.get(f"{self._base_url}/projects/{project_id}/instances?last_instance_id={last_instance_id}&batch_size={batch_size}", headers=self._headers).json()
 
     def put_prediction(self, project_id, instance_id, prediction):
-        return requests.post(f"{self._base_url}/projects/{project_id}/instances/{instance_id}/predictions", headers={"Authorization": f"Bearer {self._predictor_token}"}, json={"prediction": prediction})
+        return requests.post(f"{self._base_url}/projects/{project_id}/instances/{instance_id}/predictions", headers=self._headers, json={"prediction": prediction})
