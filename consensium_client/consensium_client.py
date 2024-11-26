8f72f53c46d8301ec118391daffcf5136bae944c
kewkie <kewkie@gmail.com>
2024-11-26 11:47:13 +0000

    Add type hints to arguments.

--- consensium_client/consensium_client.py
+++ consensium_client/consensium_client.py
@@ -23,19 +23,19 @@ class OwnerClient:
         self._base_url = base_url
         self._headers = {"Authorization": f"Bearer {self._owner_token}"}
 
-    def create_project(self, policy_document):
-        return requests.post(f"{self._base_url}/projects", headers=self._headers, json={"policy_document": policy_document}).json()
+    def create_project(self, project: dict):
+        return requests.post(f"{self._base_url}/projects", headers=self._headers, json=project).json()
 
-    def update_policy(self, project_id, policy_document):
+    def update_policy(self, project_id: str, policy_document: str):
         return requests.put(f"{self._base_url}/projects/{project_id}/policy", headers=self._headers, json={"policy_document": policy_document}).json()
     
-    def add_predictor(self, project_id, predictor_ids):
+    def add_predictor(self, project_id: str, predictor_ids: list[str]):
         return requests.put(f"{self._base_url}/projects/{project_id}/predictors", headers=self._headers, json={"predictor_ids": predictor_ids}).json()
     
-    def add_moderator(self, project_id, moderator_ids):
+    def add_moderator(self, project_id: str, moderator_ids: list[str]):
         return requests.put(f"{self._base_url}/projects/{project_id}/moderators", headers=self._headers, json={"moderator_ids": moderator_ids}).json()
 
-    def put_instance(self, project_id, features):
+    def put_instance(self, project_id: str, features: dict):
         return requests.post(f"{self._base_url}/projects/{project_id}/instances", headers=self._headers, json={"features": features}).json()
 
 
@@ -52,11 +52,11 @@ class PredictorClient:
     def list_projects(self):
         return requests.get(f"{self._base_url}/projects", headers=self._headers).json()
 
-    def get_project(self, project_id):
+    def get_project(self, project_id: str):
         return requests.get(f"{self._base_url}/projects/{project_id}", headers=self._headers).json()
 
-    def get_instance_batch(self, project_id, last_instance_id, batch_size):
+    def get_instance_batch(self, project_id: str, last_instance_id: str, batch_size: int):
         return requests.get(f"{self._base_url}/projects/{project_id}/instances?last_instance_id={last_instance_id}&batch_size={batch_size}", headers=self._headers).json()
 
-    def put_prediction(self, project_id, instance_id, prediction):
-        return requests.post(f"{self._base_url}/projects/{project_id}/instances/{instance_id}/predictions", headers=self._headers, json={"prediction": prediction})
+    def put_prediction(self, project_id: str, instance_id: str, prediction_yes: float):
+        return requests.post(f"{self._base_url}/projects/{project_id}/instances/{instance_id}/predictions", headers=self._headers, json={"prediction_yes": prediction_yes})
