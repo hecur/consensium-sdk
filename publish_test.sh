32789814ae3c3d5af33eaf977d92e4c580f734ae
kewkie <kewkie@gmail.com>
2024-11-29 11:20:46 +0000

    Clean up old builds

--- publish_test.sh
+++ publish_test.sh
@@ -28,6 +28,10 @@ fi
 git add pyproject.toml
 git commit -m "Build: increment version to ${new_version}"
 
+# Clean up old builds
+rm -rf dist/ build/ *.egg-info
+
+# Build and publish
 python -m pip install --upgrade build
 python -m build
 python -m pip install --upgrade twine
